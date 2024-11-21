790d821e37c242688c63abf33e8b924426e11582
senthur08 <manikandansenthur08@gmail.com>
2024-11-21 19:47:00 +0530

    installed whitenoise, configured static files

--- Machinetool_sales/settings.py
+++ Machinetool_sales/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,6 +137,7 @@ STATICFILES_DIRS= [
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
